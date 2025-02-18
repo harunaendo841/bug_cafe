b69098b95235bce822cf238daf058a01908805ad
harunaendo841 <h.runa0414@gmail.com>
2025-02-18 17:10:50 +0900

    変数名を具体的な名称に変更

--- cafe.rb
+++ cafe.rb
@@ -14,21 +14,21 @@ FOODS = [
   { name: 'ホットサンド', price: 410 }
 ].freeze
 
-def take_order(menus)
-  menus.each.with_index(1) do |menu, i|
-    puts "(#{i})#{menu[:name]}: #{menu[:price]}円"
+def take_order(menu_items)
+  menu_items.each.with_index(1) do |item, index|
+    puts "(#{index})#{item[:name]}: #{item[:price]}円"
   end
   print '>'
-  order_index = gets.to_i - 1
-  puts "#{menus[order_index][:name]}(#{menus[order_index][:price]}円)ですね。"
-  order_index
+  selected_index = gets.to_i - 1
+  puts "#{menu_items[selected_index][:name]}(#{menu_items[selected_index][:price]}円)ですね。"
+  selected_index
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+selected_drink_index = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
+selected_food_index = take_order(FOODS)
 
-total = DRINKS[order1][:price] + FOODS[order2][:price]
-puts "お会計は#{total}円になります。ありがとうございました！"
+total_price = DRINKS[selected_drink_index][:price] + FOODS[selected_food_index][:price]
+puts "お会計は#{total_price}円になります。ありがとうございました！"
